eb8018a678910bd6d61c2249857d417cfb5d6f71
Arty3m <kolbunartema@gmail.com>
2022-07-05 13:26:36 +0700

    fix output

--- homework2/task_1_ver2.py
+++ homework2/task_1_ver2.py
@@ -123,13 +123,12 @@ def print_result(p: list, matrix: list, fin_path: list, fin_cost: float) -> None
 
 if __name__ == "__main__":
     # points = [(0, 2), (2, 5), (5, 2), (6, 6), (8, 3)]
-    # points = [(0, 2), (2, 5), (5, 2), (6, 6), (5, 5), (8, 3)]
-    points = [(0, 0), (1, 1), (2, 2), (3, 3), (4, 4), (5, 5)]
+    points = [(0, 2), (2, 5), (5, 2), (6, 6), (5, 5), (8, 3)]
+    # points = [(0, 0), (1, 1), (2, 2), (3, 3), (4, 4), (5, 5)]
+    # points = [(0, 2), (2, 5), (5, 2), (6, 6), (20, 60), (8, 3)]
 
     # points = [(0, 1), (1, 4), (4, 1), (5, 5), (7, 2)]
     adjacent_matrix = make_matrix(points)
-    # for el in adjacent_matrix:
-    #     print(el)
 
     N = len(adjacent_matrix)
 
