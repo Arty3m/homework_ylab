e7be5413e1edbb74e96f3dcddbad29ec76b0335e
Arty3m <kolbunartema@gmail.com>
2022-07-05 12:41:48 +0700

    fixed task 1

--- homework2/task_1_ver1.py
+++ homework2/task_1_ver1.py
@@ -17,7 +17,7 @@ def print_result(r: list) -> None:
         elif i == len(tup) - 1:
             print(f'{tup[i]}[{tup[i - 1]}] = {value}')
         else:
-            print(f'{tup[i]}[{tup[i + 1]}] -> ', end='')
+            print(f'{tup[i]}[{tup[i - 1]}] -> ', end='')
 
 
 def get_shortest_route(locations: list[tuple]) -> None:
@@ -45,5 +45,9 @@ def get_shortest_route(locations: list[tuple]) -> None:
 
 if __name__ == '__main__':
     points = [(0, 2), (2, 5), (5, 2), (6, 6), (8, 3)]
+    # points = [(0, 2), (2, 5), (5, 2), (6, 6), (5, 5), (8, 3)]
+    # points = [(0, 0), (1, 1), (2, 2), (3, 3), (4, 4), (5, 5)]
     # points = [(0, 1), (1, 4), (4, 1), (5, 5), (7, 2)]
+
     get_shortest_route(points)
+
