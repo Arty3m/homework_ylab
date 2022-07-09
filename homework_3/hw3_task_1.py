38682ae973d0adf4be1828fceb1075d3484d77bf
Arty3m <kolbunartema@gmail.com>
2022-07-09 15:45:42 +0700

    decorator cach-redis

---
+++ homework_3/hw3_task_1.py
@@ -0,0 +1,37 @@
+import redis
+import time
+
+
+def cacher(func):
+    def wrapper(num, *args, **kwargs):
+        redis_client = redis.Redis()
+        cache = redis_client.get(str(num))
+        if cache is not None:
+            redis_client.close()
+            return int(cache)
+
+        result = func(num, *args, **kwargs)
+
+        redis_client.set(str(num), str(result))
+        redis_client.close()
+
+        return result
+
+    return wrapper
+
+
+@cacher
+def multiplier(number: int):
+    time.sleep(3)
+    return number * 2
+
+
+if __name__ == '__main__':
+    numbers = (2, 2, 3, 3, 5, 5)
+    for el in numbers:
+        start_t = time.perf_counter()
+
+        print('Результат вычислений:', multiplier(el))
+
+        end_t = time.perf_counter()
+        print('Выполнилось за: ', end_t - start_t, 'сек.')
